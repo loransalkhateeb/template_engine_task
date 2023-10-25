782dc3aec16f0008d0dae7286b6e9dcf8ab80113
Lorans_Alkhateeb <lwrnsalkhtyb9@gmail.com>
2023-10-25 11:58:16 -0700

    done the last modiffication

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 const express = require("express");
-const app = express();
-// const cors = require("cors");
-const port = 8000;
-const http = require('https');
-// app.use(cors());
 
-app.get("/movies", (request, response) => {
+const app = express();
+const http = require("https");
+app.set("view engine", "ejs");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+app.get("/", (request, response) => {
     const options = {
         method: 'GET',
-        hostname: 'imdb-top-100-movies.p.rapidapi.com',
+        hostname: 'imdb-top-100-movies1.p.rapidapi.com',
         port: null,
-        path: '/top100movies',
+        path: '/',
         headers: {
-            'X-RapidAPI-Key': '4d238684ecmsh29829391eb68764p182ec8jsnd5bbba0abc15',
-            'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+            'X-RapidAPI-Key': 'fe6d068457msh5750abb6609e827p1c4fb7jsn179e85a98608',
+            'X-RapidAPI-Host': 'imdb-top-100-movies1.p.rapidapi.com'
         }
     };
-    const req = http.request(options, function (res) {
-        const chunks = [];
-        res.on('data', function (chunk) {
-            chunks.push(chunk);
-        });
-        res.on('end', function () {
-            const body = Buffer.concat(chunks);
-            const moviesdata = JSON.parse(body);
-            console.log(moviesdata);
-            response.render("index.ejs", {
-                movies : moviesdata
-            });
-        });
+
+  const req = http.request(options, function (res) {
+    const chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
     });
-    req.end();
-})
+    console.log(chunks);
 
+    res.on("end", function () {
+      const body = Buffer.concat(chunks);
+      console.log(body.toString());
 
-app.listen(port, () => {
-    console.log("i am listen in port " + port);
+      const view = JSON.parse(body);
+      const filteredMovies = view.filter((movie) => movie.genre == "Drama");
+
+      response.render("index.ejs", { movie: filteredMovies });
+    });
+  });
+
+  req.end();
 });
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
